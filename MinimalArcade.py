7570cae4a8c4626fb9d2d75627df341b9f4fb786
Kevin Fung <kfung@student.bridgew.edu>
2019-09-19 22:15:11 -0400

    Updated shot sound

--- MinimalArcade.py
+++ MinimalArcade.py
@@ -118,7 +118,7 @@ class MimimalArcade(arcade.Window):
 
 def main():
     """ Main method """
-    window = MimimalArcade("PlayerShip.png", "Ocean.png", "dart.wav", "Shot.png", screen_w=1080)
+    window = MimimalArcade("PlayerShip.png", "Ocean.png", "laser4_0.wav", "Shot.png", screen_w=1080)
     window.setup()
     arcade.run()
 
